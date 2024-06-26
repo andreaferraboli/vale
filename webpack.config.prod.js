91e0cb96a66f1159782af15584f044c1aba0ae1a
andrea.ferraboli <andrew.ferro04@gmail.com>
2024-06-26 22:13:09 +0200

    first finished version

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -7,7 +7,7 @@ module.exports = merge(common, {
   mode: 'production',
   plugins: [
     new HtmlWebpackPlugin({
-      template: './index.html',
+      template: './page1.html',
     }),
     new CopyPlugin({
       patterns: [
